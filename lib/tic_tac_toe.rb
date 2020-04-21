b0f209ef6904e3cd1e5c9c8709b8ed024d6d284f
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-21 03:51:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,9 +68,7 @@ def current_player(board)
 end
 
 def won?(board)
-  WIN_COMBINATIONS.detect {|win_combo_array| board[win_combo_array[0]] == board[win_combo_array[1]] &&  board[win_combo_array[1]] == board[win_combo_array[2]] && position_taken?(board, win_combo_array[0])
-    win_combo_array
-  end
+  WIN_COMBINATIONS.detect {|win_combo_array| board[win_combo_array[0]] == board[win_combo_array[1]] &&  board[win_combo_array[1]] == board[win_combo_array[2]] && position_taken?(board, win_combo_array[0])}
 end
 
 def full?(board)
